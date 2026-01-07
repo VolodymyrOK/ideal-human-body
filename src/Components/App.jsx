047c4410b314517f4f3252bf2efb6bbf146feb04
Volodymyr <vladikon70@gmail.com>
2026-01-07 19:34:09 +0100

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -84,7 +84,7 @@ class App extends Component {
         }}
       >
         <div className="container">
-          <Header>Проектик для тренировок (HTML, CSS, JS, REACT)</Header>
+          <Header>Проектик для тренировок (HTML, CSS, JS, REACT, GIT)</Header>
           <Main>
             {isShowMainPage && (
               <MainPage
